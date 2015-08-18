6a73ea8d6453c57745568940289e486cf76ca1c9
yudesong <yudesong@foxmail.com>
2015-08-18 15:31:15 +0800

    modify

--- routes/blog.js
+++ routes/blog.js
@@ -15,7 +15,17 @@ router.get('/', function(req, res, next) {
 
 router.fabu = function(req,res){
      var content = req.body.content;
-     collection.insert({"images":"images/user01.jpg","name":"牛客网","content":content,"images_1":"images/b2.jpg","images_1_1":"images/m2.jpg"},function(err,docs){
+     var file = req.body.file;
+     console.log(file);
+     content = content.replace(/\[高兴\]/g,"<img src='images/mr/0.gif' alt='测试' title='测试'>");
+     content = content.replace(/\[给力\]/g,"<img src='images/mr/1.gif' alt='测试' title='测试'>");
+     content = content.replace(/\[色\]/g,"<img src='images/mr/2.gif' alt='测试' title='测试'>");
+     content = content.replace(/\[恭喜发财\]/g,"<img src='images/mr/3.gif' alt='测试' title='测试'>");
+     content = content.replace(/\[强\]/g,"<img src='images/mr/4.gif' alt='测试' title='测试'>");
+     content +="</br><a class='miniImg artZoom' href='images/"+file+"'";
+     content +=" rel='images/"+file+"'>";
+     content += "<img src='images/"+"m"+file.substring(1)+"'/></a>"; 
+     collection.insert({"images":"images/user01.jpg","name":"牛客网","content":content},function(err,docs){
           if(!err){
                if(docs!=""){
                     res.redirect("blog");
